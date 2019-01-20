0e228c19d6f2e64b64d8aa72b428208a58007329
arty <art.yerkes@gmail.com>
2019-01-19 18:51:36 -0800

    Fix codegen name

--- mk_tests.sh
+++ mk_tests.sh
@@ -2,5 +2,5 @@ for f in tests/*.idr
 do
   echo compiling $f
   rm ${f%%.idr}.ibc 2>/dev/null
-  idris -p js  --codegen js $f  -o ${f%%.idr}.js
+  idris -p js -p contrib  --codegen javascript $f  -o ${f%%.idr}.js
 done
