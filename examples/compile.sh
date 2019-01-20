19ac0bce849e3e2c577ff645c2de4556ab4be382
arty <art.yerkes@gmail.com>
2019-01-19 18:54:47 -0800

    Deps

--- examples/compile.sh
+++ examples/compile.sh
@@ -1,3 +1,3 @@
 export IDRISJS_OPTIM=$2
 export IDRISJS_DEBUG=$3
-idris -p js -p effects -p contrib --codegen javascript  $1 -o ${1%%.idr}.html
+idris -p js -p effects -p contrib -p containers --codegen javascript  $1 -o ${1%%.idr}.html
